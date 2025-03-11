d813bbedb1d8c85ab3e9b71e344474259207d426
Yousef-Hegazy <yousefashraf14725@gmail.com>
2025-03-11 23:40:23 +0300

    Initial Commit

---
+++ src/main/java/com/eazy_bank/eazy_bank/controllers/TransactionsController.java
@@ -0,0 +1,4 @@
+package com.eazy_bank.eazy_bank.controllers;
+
+public class TransactionsController {
+}
---
+++ src/test/java/com/eazy_bank/eazy_bank/EazyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazy_bank.eazy_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
